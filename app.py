eff5894efc505b9c5ae3257ed1c0e15c4e6a4852
Fol <970550793@qq.com>
2020-12-21 00:36:26 +0800

    update

--- app.py
+++ app.py
@@ -1,108 +1,135 @@
 import os
-from flask import Flask,request,jsonify,render_template
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS  # 解决跨域的问题
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from config import Config, networkSeg
 from gethost import getIP
 import time
 import threading
 app = Flask(__name__)
 
-userIP,userAR = getIP()
+# 连接用户更新间隔时间
+updateTime = 20
 
-#多线程，持续更新在线列表
+userIP, userAR = getIP()
+
+# 多线程，持续更新在线列表
+
+
 def getDevice(t):
-    global userIP,userAR
+    global userIP, userAR
     while True:
-        userIP,userAR = getIP()
+        userIP, userAR = getIP()
         print('列表更新')
         time.sleep(t)
 
-#导入数据库配置
+
+# 导入数据库配置
 app.config.from_object(Config)
 # 建立数据库对象
 db = SQLAlchemy(app)
- # 初始化数据库链接
-db.init_app(app) 
+# 初始化数据库链接
+db.init_app(app)
 
 CORS(app, supports_credentials=True)
 
-#数据库类定义
+# 数据库类定义
+
+
 class UDPCnt(db.Model):
     # 定义表名
     __tablename__ = 'UDP流量统计'
     # 定义字段对象
     count = db.Column(db.Integer)
-    ip = db.Column(db.String(255),primary_key=True)
+    IP = db.Column(db.String(255), primary_key=True) 
     # 用于打印模型对象时显示的字符串信息
+
     def __repr__(self):
-        return 'UDPcnt:%s %d'% (self.ip, self.count)
+        return 'UDPcnt:%s %d' % (self.ip, self.count)
+
 
 class TCPCnt(db.Model):
     __tablename__ = 'TCP流量统计'
     count = db.Column(db.Integer)
-    ip = db.Column(db.String(255),primary_key=True)
+    IP = db.Column(db.String(255), primary_key=True)
+
     def __repr__(self):
-        return 'TCPcnt:%s %d'% (self.ip, self.count)
+        return 'TCPcnt:%s %d' % (self.ip, self.count)
+
 
 class DirtyWordSet(db.Model):
     __tablename__ = '敏感词词库'
-    No = db.Column(db.Integer,primary_key=True)
-    敏感词 = db.Column(db.String(255),comment='敏感词')
+    No = db.Column(db.Integer, primary_key=True)
+    敏感词 = db.Column(db.String(255), comment='敏感词')
     时间 = db.Column(db.String(255))
+
     def __repr__(self):
-        return 'Dirty word set:%s'% (self.敏感词)
+        return 'Dirty word set:%s' % (self.敏感词)
+
 
 class DirtyWordInfo(db.Model):
     __tablename__ = '敏感词记录'
-    No = db.Column(db.Integer,primary_key=True)
-    来源ip = db.Column(db.String(255),comment='来源ip')
-    敏感词 = db.Column(db.String(255),comment='敏感词')
-    时间 = db.Column(db.String(255),comment='时间')
+    No = db.Column(db.Integer, primary_key=True)
+    来源ip = db.Column(db.String(255), comment='来源ip')
+    敏感词 = db.Column(db.String(255), comment='敏感词')
+    时间 = db.Column(db.String(255), comment='时间')
+
     def __repr__(self):
-        return 'Dirty word record:%s %s %s'% (self.来源ip, self.敏感词, self.时间)
+        return 'Dirty word record:%s %s %s' % (self.来源ip, self.敏感词, self.时间)
+
+class ConnectedDevice(db.Model):
+    __tablename__ = '当前设备'
+    No = db.Column(db.Integer, primary_key=True)
+    物理地址 = db.Column(db.String(255))
+    ip = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '设备地址:%s' % (self.物理地址)
 
 
 # 返回udp流量统计信息
 @app.route('/cnt/udp', methods=["GET"])
 def get_cnt_udp():
-    alldata = UDPCnt.query.all()
     json = []
-    tempdata = {}
-    for data in alldata:
-        if data.ip in userIP:
-            tempdata = {}
-            tempdata['ip'] = data.ip
-            tempdata['count'] = data.count
-            json.append(tempdata)
+    tempdata = []
+    for ip in userIP:
+        data = UDPCnt.query.filter(UDPCnt.IP == ip).first()
+        if data:
+            tempdata.append(data.count)
+        else:
+            tempdata.append(0)
+    json = [userIP, tempdata]
     return jsonify(json)
 
 # 返回tcp流量统计信息
 @app.route('/cnt/tcp', methods=["GET"])
 def get_cnt_tcp():
-    alldata = TCPCnt.query.all()
     json = []
-    tempdata = {}
-    for data in alldata:
-        if data.ip in userIP:
-            tempdata = {}
-            tempdata['ip'] = data.ip
-            tempdata['count'] = data.count
-            json.append(tempdata)
+    tempdata = []
+    for ip in userIP:
+        data = TCPCnt.query.filter(TCPCnt.IP == ip).first()
+        if data:
+            tempdata.append(data.count)
+        else:
+            tempdata.append(0) 
+    json = [userIP, tempdata]
     return jsonify(json)
 
 # 返回敏感词检测信息
+
+
 @app.route('/dirtyword', methods=["GET"])
 def get_dirtyword_info():
     alldata = DirtyWordInfo.query.all()
     json = []
     tempdata = {}
     for data in alldata:
-        tempdata['dirtyword'] = data.敏感词
-        tempdata['sourceip'] = data.来源ip
-        tempdata['time'] = data.时间
-        json.append(tempdata)
-        tempdata = {}
+        if networkSeg in data.来源ip:
+            tempdata = {}
+            tempdata['dirtyword'] = data.敏感词
+            tempdata['sourceip'] = data.来源ip
+            tempdata['time'] = data.时间
+            json.append(tempdata)
     return jsonify(json)
 
 
@@ -131,7 +158,8 @@ def get_dirtyword():
     return jsonify(json)
 
 
-# 返回接入用户信息
+# 返回接入用户信息（本地版）
+'''
 @app.route('/user_data', methods=["GET"])
 def get_user_data():
     data = []
@@ -140,25 +168,28 @@ def get_user_data():
         dic["ip"] = userIP[i]
         dic["address"] = userAR[i]
         data.append(dic)
-
-    '''
-    data = [{
-          'time': '2020-11-18',
-          'username': 'Nuo',
-          'ip': '192.168.0.1'
-        },
-        {
-            'time': '2020-11-18',
-            'username': 'Nuo',
-            'ip': '192.168.0.1'
-        },
-    ]
-    '''
     return jsonify(data)
+'''
+
+# 返回用户接入信息（数据库）
+@app.route('/user_data', methods=["GET"])
+def get_user_data():
+    alldata = ConnectedDevice.query.all()
+    json = []
+    userIP = []
+    userAR = []
+    for data in alldata:
+        dic = {}
+        dic["ip"] = data.ip
+        userIP.append(data.ip)
+        dic["address"] = data.物理地址
+        userAR.append(data.物理地址)
+        json.append(dic)
+    return jsonify(json)
 
 
 if __name__ == '__main__':
-    th_get = threading.Thread(target=getDevice, args=(15,))
-    th_get.start()
+    #th_get = threading.Thread(target=getDevice, args=(updateTime,))
+    #th_get.start()
 
     app.run(debug=True)
